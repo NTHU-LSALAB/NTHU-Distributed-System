4710a0feb08778df932f46b6bc554ac88da591fe
justin0u0 <mail@justin0u0.com>
2022-03-05 02:19:29 +0800

    feat: generate video/pb VideoClient mock

--- modules/video/mock/pbmock/main.go
+++ modules/video/mock/pbmock/main.go
@@ -1,3 +1,3 @@
 package pbmock
 
-//go:generate mockgen -destination=mock.go -package=$GOPACKAGE github.com/NTHU-LSALAB/NTHU-Distributed-System/modules/video/pb Video_UploadVideoServer
+//go:generate mockgen -destination=mock.go -package=$GOPACKAGE github.com/NTHU-LSALAB/NTHU-Distributed-System/modules/video/pb Video_UploadVideoServer,VideoClient
