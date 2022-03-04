ff7c10a8b715b9ebf9b2455605a42a405ab085f7
justin0u0 <mail@justin0u0.com>
2022-03-05 02:18:38 +0800

    feat: implement grpckit GrpcClientConn

---
+++ pkg/grpckit/client_conn.go
@@ -0,0 +1,53 @@
+package grpckit
+
+import (
+	"context"
+	"time"
+
+	"github.com/NTHU-LSALAB/NTHU-Distributed-System/pkg/logkit"
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type GrpcClientConnConfig struct {
+	Timeout    time.Duration `long:"timeout" env:"TIMEOUT" default:"30s"`
+	ServerAddr string        `long:"server_addr" env:"SERVER_ADDR" required:"true"`
+}
+
+type GrpcClientConn struct {
+	*grpc.ClientConn
+
+	closeFunc func()
+}
+
+func (c *GrpcClientConn) Close() error {
+	if c.closeFunc != nil {
+		c.closeFunc()
+	}
+
+	return c.ClientConn.Close()
+}
+
+func NewGrpcClientConn(ctx context.Context, conf *GrpcClientConnConfig) *GrpcClientConn {
+	logger := logkit.FromContext(ctx).With(
+		zap.String("server_addr", conf.ServerAddr),
+	)
+
+	var cancel context.CancelFunc
+	ctx, cancel = context.WithTimeout(ctx, conf.Timeout)
+
+	conn, err := grpc.DialContext(ctx, conf.ServerAddr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		logger.Fatal("failed to connect to gRPC server", zap.Error(err))
+	}
+
+	closeFunc := func() {
+		cancel()
+	}
+
+	return &GrpcClientConn{
+		ClientConn: conn,
+		closeFunc:  closeFunc,
+	}
+}
