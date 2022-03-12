2ef5d8b6636caf154497b72c65d26af39b5b15fa
Kerwin Tsai <kerwintsai@pm.me>
2022-03-12 22:11:59 +0800

    Fix context bug when testing.

--- pkg/mongokit/mongokit_test.go
+++ pkg/mongokit/mongokit_test.go
@@ -4,25 +4,39 @@ import (
 	"context"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/NTHU-LSALAB/NTHU-Distributed-System/pkg/logkit"
 )
 
 var _ = Describe("Mongokit", func() {
 	Describe("NewMongoClient", func() {
 		var (
-			mongoclient *MongoClient
+			mongoClient *MongoClient
 			ctx context.Context
+			mongoConfig *MongoConfig
 		)
 
-		JustBeforeEach(func() {
-			mongoclient = NewMongoClient(ctx, &MongoConfig{
+		BeforeEach(func() {
+			ctx = logkit.NewLogger(&logkit.LoggerConfig{
+				Development: true,
+			}).WithContext(context.Background())
+
+			mongoConfig = &MongoConfig{
 				URL:      "mongodb://mongo:27017",
 				Database: "video",
-			})
+			}
+		})
+
+		JustBeforeEach(func() {
+			mongoClient = NewMongoClient(ctx, mongoConfig)
+		})
+
+		AfterEach(func() {
+			Expect(mongoClient.Close()).NotTo(HaveOccurred())
 		})
 
 		When("success", func() {
 			It("returns new Mongokit without error", func() {
-				Expect(mongoClient.Close()).NotTo(HaveOccurred())
+				Expect(mongoClient).NotTo(BeNil())
 			})
 		})
 	})
